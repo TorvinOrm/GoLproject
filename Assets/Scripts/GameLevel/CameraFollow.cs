cc2577be34f45b22210606add3769610d4e0f0c9
DecimusScaevola <kozachenko.ivan@gmail.com>
2018-02-25 19:00:28 +0200

    Movement script for character (no animation)
    Scripts:
    - Movement (forward/backward and jump)
    
    Assets:
    - Dummy character (with rigidbody)

---
+++ Assets/Scripts/GameLevel/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject levelCamera;
+	Vector3 cameraPosition;
+
+	// Use this for initialization
+	/// <summary>
+	/// Use this to get the distance between camera and player
+	/// </summary>
+	void Start () {
+		cameraPosition = levelCamera.GetComponent<Transform> ().position - player.GetComponent<Transform> ().position;
+	}
+	
+	/// <summary>
+	/// Update camera position and hold in against the player
+	/// </summary>
+	void Update () {
+		levelCamera.transform.position = player.transform.position + cameraPosition;
+	}
+}
